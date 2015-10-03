f455a99ada4de461ccc31e7763d62d0997a80003
timzchang <tchang2@nd.edu>
2015-10-03 16:28:35 -0400

    upadted parse.js

--- parse-js-blank/public/js/parse.js
+++ parse-js-blank/public/js/parse.js
@@ -1,12 +1,10 @@
 Parse.initialize("cSHa81oGKG1Y3iVayTf5EfmrQ8DLt0Nb8797hJa3", "ZtZHoIzn1IfXu68c65mMikShyhXbiwu1Gv7VNa4g");
-
 var UserProfile = Parse.Object.extend("UserProfile");
-
 var hp = 99;
 
 
 $("#test-button").click(function() {
-	// alert("Ya clicked me");
+	alert("Ya clicked me");
 	var userProfile = new UserProfile();
 	userProfile.save({name: "Tim", health: hp--, sanity: 25}, {
 		success: function(object) {
@@ -30,15 +28,15 @@ function getUser(){
 	var query = new Parse.Query(UserProfile);
 	// query.equalTo("health",100);
 	query.find({
-		success: function(result){
+		success: function(results){
 			console.log("found");
-			for(var i=0;i<result.length;i++){
-				result[i].destroy(function () {
-					success: function(){
-						console.log("destroyed");
+			for(var i=0;i<results.length;i++){
+				results[i].destroy({
+					success: function(object){
+						console.log("destroyed object");
 					},
-					error: function(){
-						console.log("can't destory");
+					error: function(model, error){
+						console.log("couldn't destroy");
 					}
 				});
 			}

fcf306b4db14138ea0350c39b92ae29bbaa9ee88
jhuseman <jhuseman@nd.edu>
2015-10-03 17:15:06 -0400

    added up and down arrow toggle for side bars

--- parse-js-blank/public/js/popout.js
+++ parse-js-blank/public/js/popout.js
@@ -1,24 +1,44 @@
 var friendsout = 0;
 var locationout = 0;
 
+function togglefriends() {
+	if(friendsout) {
+		hidefriends();
+	} else {
+		popoutfriends();
+	}
+}
+
+function togglelocation() {
+	if(locationout) {
+		hidelocation();
+	} else {
+		popoutlocation();
+	}
+}
+
 function popoutfriends() {
 	friendsout = 1;
 	popout("friends");
+	document.getElementById("friendarrow").src = "images/upArrow.png";
 };
 
 function popoutlocation() {
 	locationout = 1;
 	popout("locations");
+	document.getElementById("locarrow").src = "images/upArrow.png";
 };
 
 function hidefriends() {
 	friendsout = 0;
 	hide("friends");
+	document.getElementById("friendarrow").src = "images/dnArrow.png";
 };
 
 function hidelocation() {
 	locationout = 0;
 	hide("locations");
+	document.getElementById("locarrow").src = "images/dnArrow.png";
 };
 
 function popout(id) {
